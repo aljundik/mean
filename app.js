338b711b472b6e208ed25b03febe9512ebf9f041
aljundik <kaledj007@gmail.com>
2016-09-20 23:20:52 +0300

    Listen to ports and setting up express

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("it Works :))) ");
+var express = require('express'); // import express
+var app = express();// assigh express to the variable
+
+app.set('port',3000); // set up the port to be 3000
+var server = app.listen(app.get('port'),function(){
+	var port = server.address().port;// defin a variable port and assign the current addres to it
+	console.log("the port is " + port);
+});// set the app to listen to the retrieved port and run the statment after conferming that it is runnin (call back function)
