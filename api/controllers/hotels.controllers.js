f82f9bdafe222353ddf8744b0158855eceeafdbb
aljundik <kaledj007@gmail.com>
2016-09-30 19:07:10 +0300

    Add routes and controllers

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,7 +1,51 @@
+var hotelData = require('../data/hotel-data.json');
 module.exports.hotelsGetAll = function(req,res){
-	console.log("we are in the hotels page page GET");
+	console.log("we are in the hotels page GET");
+
+
+	var offset = 0;
+	var count =5; 
+
+	if (req.query && req.query.offset){// check if query(URL) exist, and then check if it does hold the value offset
+		offset= parseInt (req.query.offset,10);//since the request url is string, we need to change it to number by using parse int and 10 is to represent the numerical system used, in this case it is a decimal value
+	}
+	if (req.query && req.query.count){// check if query(URL) exist, and then check if it does hold the value count
+		count= parseInt (req.query.count,10);//since the request url is string, we need to change it to number by using parse int
+	}
+
+	var returnData = hotelData.slice(offset,offset+count);
+
 	res
 		.status(200)
-		.json({"json" : true});
+		.json(returnData);
 
 };
+
+module.exports.hotelsGetOne = function(req,res){
+	var hotelId = req.params.hotelId;// req parameters handles the url partamters 
+	var thisHotel = hotelData[hotelId];// get that particular item from the array hotelData(json)
+	console.log("get hotelID", hotelId);
+	res
+		.status(200)
+		.json(thisHotel);
+
+};
+
+module.exports.hotellsAddOne = function(req,res){
+	console.log("post new hotel");
+	console.log(req.body);// will put all the data coming from the posted form to the log using the middlewar tools we defined in app.js
+
+	res
+		.status(200)
+		.json(req.body);
+
+
+
+
+};
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 	var express = require('express'); // import express
 	var app = express();// assigh express to the variable
 	var path = require('path');// import path module (to deal with files)
+	var bodyParser = require('body-parser');
 
 	var routes = require('./api/routes')
 	app.set('port',3000); // set up the port to be 3000
@@ -12,8 +13,10 @@
 		next();// middleware handle functions needed to be run between request and respond
 	});
 
-
+		//test
+	
 	app.use(express.static(path.join(__dirname, 'public')));//define a static path for the routes
+	app.use(bodyParser.urlencoded({extended : false }));// only string and json,, this is a middleware to deal with requests paramaeters
 	app.use('/api',routes);
 
 
