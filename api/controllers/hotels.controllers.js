f8b95a850caf875a2ad56bed4fcbcaafda1a290e
aljundik <kaledj007@gmail.com>
2016-10-04 00:12:22 +0300

    Add to mongo database

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,4 +1,5 @@
 var dbconn = require ('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 
@@ -6,11 +7,7 @@ var hotelData = require('../data/hotel-data.json');
 module.exports.hotelsGetAll = function(req,res){
 	
 	var db = dbconn.get();
-
-	console.log("db", db);
-	
-	console.log("we are in the hotels page GET");
-
+	var collection = db.collection('hotels');
 
 	var offset = 0;
 	var count =5; 
@@ -22,31 +19,73 @@ module.exports.hotelsGetAll = function(req,res){
 		count= parseInt (req.query.count,10);//since the request url is string, we need to change it to number by using parse int
 	}
 
-	var returnData = hotelData.slice(offset,offset+count);
+	collection
 
-	res
+	.find()
+	.skip(offset)
+	.limit(count)
+	.toArray(function(err,docs){
+
+		console.log("Found hotels", docs);
+		res
 		.status(200)
-		.json(returnData);
+		.json(docs);
+	});
 
 };
 
 module.exports.hotelsGetOne = function(req,res){
-	var hotelId = req.params.hotelId;// req parameters handles the url partamters 
-	var thisHotel = hotelData[hotelId];// get that particular item from the array hotelData(json)
-	console.log("get hotelID", hotelId);
-	res
-		.status(200)
-		.json(thisHotel);
+	var db = dbconn.get();
+	var collection = db.collection('hotels');
 
-};
+		var hotelId = req.params.hotelId;// req parameters handles the url partamters 
+		collection
+		.findOne({
+			_id : ObjectId(hotelId)
+		},function(err,doc){
 
-module.exports.hotellsAddOne = function(req,res){
-	console.log("post new hotel");
-	console.log(req.body);// will put all the data coming from the posted form to the log using the middlewar tools we defined in app.js
+			res
+			.status(200)
+			.json(doc);
+		});
+		console.log("get hotelID", hotelId);
+		
+	};
 
-	res
-		.status(200)
-		.json(req.body);
+	module.exports.hotellsAddOne = function(req,res){
+		var db = dbconn.get();
+		var collection = db.collection('hotels');
+		var newHotel;
+
+		console.log("post new hotel");
+
+		if(req.body && req.body.name && req.body.stars){
+			newHotel = req.body;
+			newHotel.stars = parseInt(req.body.stars,10);
+			console.log(newHotel);// will put all the data coming from the posted form to the log using the middlewar tools we defined in app.js
+
+
+			collection.insertOne(newHotel,function(err,response){
+
+				console.log(response.ops);
+				res
+					.status(201)
+					.json(response.ops); 
+
+			});
+
+			
+
+		}
+		else {
+			console.log("Data missing from body");
+			res
+			.status(400)
+			.json({message : "Required data missing from body"});
+		}
+
+
+		
 	};
 
 
